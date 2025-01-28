f347f45296054f44330005e5661d94408ecd0e0e
SRN <soumyaranjannath41@gmail.com>
2025-01-28 15:47:50 +0530

    removed employees id from select inside SP_GetEmployeesDetails

--- src/main/resources/db/sps/SP_GetEmployeesDetails.sql
+++ src/main/resources/db/sps/SP_GetEmployeesDetails.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset soumya:creates get employee details sp
+--changeset soumya:removed e.EmployeeId 
 --comment comments from eclipse ide
 
 
@@ -8,7 +8,7 @@ create procedure [dbo].[SP_Get_EmployeeDetails]
 			@DepartId int
 			as
 			begin
-			select e.EmployeeId,e.EmployeeName,e.Position,
+			select e.EmployeeName,e.Position,
 	d.DepartmentName,p.ProjectName,p.StartDate,p.EndDate
 	from Employees e join Departments d on e.DepartmentId=d.DepartmentId
 	join Projects p on e.ProjectId=p.ProjectId and e.DepartmentId=@DepartId		
