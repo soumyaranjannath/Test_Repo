caf8dfe89508c960927d8ed0bfec5ef97cb9ec48
SRN <soumyaranjannath41@gmail.com>
2025-01-28 17:35:33 +0530

    Removed one column from sp

--- src/main/resources/db/sps/SP_GetEmployeesDetails.sql
+++ src/main/resources/db/sps/SP_GetEmployeesDetails.sql
@@ -8,7 +8,7 @@ alter procedure [dbo].[SP_Get_EmployeeDetails]
 			@DepartId int
 			as
 			begin
-			select e.EmployeeName,e.Position,
+			select e.EmployeeName,
 	d.DepartmentName,p.ProjectName,p.StartDate,p.EndDate
 	from Employees e join Departments d on e.DepartmentId=d.DepartmentId
 	join Projects p on e.ProjectId=p.ProjectId and e.DepartmentId=@DepartId		
